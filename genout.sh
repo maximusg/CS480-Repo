d2eca163826de07bab0867ef7937a1aab777a37c
Richard Tracy <thebigcheeze@gmail.com>
2010-01-20 18:59:10 -0800

    added genout.sh, a thing to make all the output files

---
+++ genout.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/sh
+
+java Asgn2 test1 > test1.o
+java Asgn2 test2 > test2.o
+java Asgn2 test3 > test3.o
+java Asgn2 test4 > test4.o
+java Asgn2 test5 > test5.o
+java Asgn2 test6 > test6.o
+java Asgn2 test7 > test7.o
+java Asgn2 test8 > test8.o
+java Asgn2 test9 > test9.o
+java Asgn2 test10 > test10.o
+java Asgn2 test11 > test11.o
+java Asgn2 testn > testn.o
+java Asgn2 testm > testm.o
